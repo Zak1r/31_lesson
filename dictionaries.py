9cf144d5b0b4739f87056c4392c275aec6f6ad2f
Zak1r <94119532+Zak1r@users.noreply.github.com>
2022-03-04 20:58:12 +0600

    solved calculator

---
+++ dictionaries.py
@@ -0,0 +1,80 @@
+# from calendar import month
+
+
+# months = {
+#     'Janaury': 31,
+#     'February': 28,
+#     'March': 31,
+#     'April': 30,
+#     'May': 31, 
+#     'June': 30, 
+#     'July': 31,
+#     'August': 31,
+#     'Septemper': 30,
+#     'October': 31,
+#     'November': 30,
+#     'December': 31
+
+# }
+
+# months = {
+#     1: 31,
+#     2: 28,
+#     2: 30
+# }
+
+# print(months)
+
+# months_list = [31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30, 31]
+
+
+# def calculator(num1, num2, action):
+#     if action == 'add':
+#         print(num1 + num2)
+#     elif action == 'substract':
+#         print(num1 - num2)
+#     elif action == 'multiply':
+#         print(num1 * num2)
+#     elif action == 'divide':
+#         print(num1 / num2)
+
+# print(calculator(1, 5, 'add'))
+# print(calculator(1, 5, 'substruct'))
+# print(calculator(1, 5, 'multiply'))
+# print(calculator(1, 5, 'divide'))
+
+# calculator(1, 5, 'add') -> 6
+# calculator(1, 5, 'substruct') -> -4
+# calculator(1, 5, 'multiply') -> 5
+# calculator(1, 5, 'divide') -> 1/5 = 0.2
+
+# signs = {
+#     'add': '+', 'subtract': '-', 'multiply': '*',  'divide': '/'
+
+# }
+
+# def calculator(n1, n2, action):
+#     return eval(f'{n1}{signs[action]}{n2}')
+
+# print(calculator(1, 5, 'add'))
+# print(calculator(1, 5, 'subtract'))
+# print(calculator(1, 5, 'multiply'))
+# print(calculator(1, 5, 'divide'))
+
+
+# signs = {
+#     'add': '+', 'subtract': '-', 'multiply': '*',  'divide': '/'
+
+# }
+
+# def calculator(n1, n2, action):
+#     return eval(f'{n1}{signs[action]}{n2}')
+
+# print(calculator(1, 5, 'add'))
+# print(calculator(1, 5, 'subtract'))
+# print(calculator(1, 5, 'multiply'))
+# print(calculator(1, 5, 'divide'))
+
+
+
+
