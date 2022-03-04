33a78fb395bb274f783386fbc8c4d58d24baaa34
Zak1r <94119532+Zak1r@users.noreply.github.com>
2022-03-04 20:24:03 +0600

    Spisok tema zakonchilas i created dictionary.py

---
+++ index.py
@@ -0,0 +1,47 @@
+#  SPISKI
+
+# def abc():
+    # pass
+
+# abc()
+
+
+# students = [1, 2, 3, 4, 'Umar' 'Abduvali', 'Iskandar', 'Sardor', 'Ramziddin']
+
+# students = ['Umar', 'Abduvali', 'Iskandar', 'Sardor', 'Ramziddin']
+
+# students.reverse()  # obratniy poryadok  
+# students.pop() # Udalyayet po indeksu
+
+# print(students)
+
+# print('Popped element: ', students.pop(4))
+
+# print('Students list: ', students.remove('Abduvali')) # udalyayet po znacheniyu
+
+
+# print('Students list: ', students.append('Xasan'))  # dobalyayet tolko 1 spisok
+
+# print('Students list: ', students.extend(['Zakir', 'Zafar', 2 + 3, '4 + 1'])) # dobavlyayet mojno mnogo raz
+
+# print('Students list: ', students.insert(3, 'Dilnura')) # dobavlyayet po indeksu
+
+# print('Students list: ', students.sort()) # sortiruet po alfavitu libo po zacheniyu
+
+# print('Students list: ', students.sort(reverse = True)) # reverse = True sortiruet po obratnom poryadke
+
+# print('Students list: ', students.clear())  #polnostyu ochishaet
+
+# print('Students list: ', students.count('Umar')) #poschitayet spisok
+
+# print('Students list: ', students.index('Sardor')) #ishet indeks po spisku
+
+# del students [3]  # udalyayet po indeksu  -->  del object[index]
+
+
+
+# print('Students list: ', students)
+
+
+
+
